48399aa70167153bb2cd367a1c4b1ee8d1fe7cde
touria2001 <77863089+touria2001@users.noreply.github.com>
2021-12-15 00:33:17 +0100

    user

--- MyApplication4/app/src/main/java/com/example/myapplication4/messageActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication4/messageActivity.java
@@ -2,32 +2,55 @@ package com.example.myapplication4;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.myapplication4.ViewHolder.MessageViewHolder;
+import com.example.myapplication4.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class messageActivity extends AppCompatActivity {
+import Model.Products;
+import Model.message;
 
-    TextView messageRecu;
+public class messageActivity extends AppCompatActivity {
+    private DatabaseReference MessagesRef;
+
+    TextView ourUser;
+    //  TextView messageRecu;
     DatabaseReference reference;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        messageRecu=(TextView) findViewById(R.id.messageRecu);
+        MessagesRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        recyclerView = findViewById(R.id.recycler_menuChat);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+      /*  messageRecu=(TextView) findViewById(R.id.messageRecu);
 
         reference= FirebaseDatabase.getInstance().getReference("Users").child(getIntent().getExtras().get("loggedUser").toString()).child("message");
 //String message=reference.getValue(String.class);
-//        messageRecu.setText(message );
+//        messageRecu.setText(message );*/
 
-        reference.addValueEventListener(new ValueEventListener() {
+     /*   reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String message=snapshot.getValue(String.class);
@@ -38,10 +61,38 @@ public class messageActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        });
+        });*/
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<message> options =
+                new FirebaseRecyclerOptions.Builder<message>()
+                        .setQuery(MessagesRef, message.class)
+                        .build();
+        FirebaseRecyclerAdapter<message, MessageViewHolder> adapter =
+                new FirebaseRecyclerAdapter<message, MessageViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull MessageViewHolder holder, int position, @NonNull message model) {
+
+                        holder.ourUser.setText(model.getPhone());
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat, parent, false);
+                        MessageViewHolder holder = new MessageViewHolder(view);
+                        return holder;
+                    }};
+
+//    public void repondre(View view) {
+//    }
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 
-    public void repondre(View view) {
-    }
+
 
 }
