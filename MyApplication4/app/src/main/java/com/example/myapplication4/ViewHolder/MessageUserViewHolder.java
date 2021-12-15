e46fcc633c115775e9f72f5829b98aad97c56386
touria2001 <77863089+touria2001@users.noreply.github.com>
2021-12-15 14:04:11 +0100

    lll

---
+++ MyApplication4/app/src/main/java/com/example/myapplication4/ViewHolder/MessageUserViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.myapplication4.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication4.Interface.ItemClickListner;
+import com.example.myapplication4.R;
+
+public class MessageUserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView ourMsg;
+    public ItemClickListner listner;
+
+
+
+
+
+
+    public MessageUserViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ourMsg = (TextView) itemView.findViewById(R.id.ourMsg);
+
+
+
+
+    }
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner= listner;
+    }
+
+    @Override
+    public void onClick(View view) {
+        listner.onClick(view, getAdapterPosition(),false);
+    }
+
+}
