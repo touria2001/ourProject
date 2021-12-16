db05755842c81d839d5be5ef558d1eff4a01572b
touria2001 <77863089+touria2001@users.noreply.github.com>
2021-12-16 02:26:31 +0100

    jyutffy

--- MyApplication4/app/src/main/java/com/example/myapplication4/messageActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication4/messageActivity.java
@@ -22,10 +22,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import Model.Products;
 import Model.message;
 import Prevalent.Prevalent;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class messageActivity extends AppCompatActivity {
     private DatabaseReference MessagesRef;
@@ -40,11 +42,15 @@ private String ourUserActuel;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         MessagesRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        CircleImageView profileImageView = (CircleImageView) findViewById(R.id.ourUserImg);
+        Picasso.get().load(Prevalent.currentOnLineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
+
 
         recyclerView = findViewById(R.id.recycler_menuChat);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
 
       /*  messageRecu=(TextView) findViewById(R.id.messageRecu);
 
@@ -79,6 +85,7 @@ private String ourUserActuel;
 
                         holder.ourUser.setText(model.getPhone());
                         ourUserActuel = model.getPhone();
+                        Picasso.get().load(model.getImg()).into(holder.ourUserImg);
 
                     }
 
